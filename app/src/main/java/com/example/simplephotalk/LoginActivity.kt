f2565e6a9544f302fc7f0a83351cd164864582ee
metametamong <113857362+metametamong@users.noreply.github.com>
2022-11-19 05:59:02 +0900

    Update LoginActivity.kt

--- app/src/main/java/com/example/simplephotalk/LoginActivity.kt
+++ app/src/main/java/com/example/simplephotalk/LoginActivity.kt
@@ -53,6 +53,11 @@ class LoginActivity : AppCompatActivity() {
         printHashKey()
     }
 
+    override fun onStart() {
+        super.onStart()
+        moveMainPage(auth?.currentUser)
+    }
+
     fun printHashKey() {
         try {
             val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
